2e77be993e9fd1d18e904bfbe6ab2045ebac5055
yennergonzalez <85211838+yennergonzalez@users.noreply.github.com>
2022-09-17 22:11:43 -0600

    Inclusión de comentarios adicionales

--- Semaforo.c
+++ Semaforo.c
@@ -24,7 +24,7 @@ int main()
     // Configuracion timers
     TIMSK = 0x01;   // Habilitar comparacion con A
     TCCR0A = 0x02;  // Configurar modo de operacion normal, sin PWM
-    TCCR0B = 0x05;   // Seleccionar frecuencia de reloj con prescaler de 1024 (frecuencia efectiva de 7812.5 Hz)
+    TCCR0B = 0x05;  // Seleccionar frecuencia de reloj con prescaler de 1024 (frecuencia efectiva de 7812.5 Hz)
     OCR0A = 0x4E;   // Se almacena el numero a comparar con el timer para generar la excepcion 
     
     //  FSM del programa
@@ -45,29 +45,28 @@ int main()
                 }
                 else
                 {
-                    if (boton_presionado == 1)
+                    if (boton_presionado == 1)  // Se pasa al estado 1 cuando se alcancen 10 segundos y se haya presionado el botón previamente
                     {
                         state = 1;  
-                        boton_presionado = 0;
+                        boton_presionado = 0;   // Se restablece el estado del botón a no presionado
                     }
                     else
                     {
                         state = 0;
                     }
-                    count = 0;
+                    count = 0;      // Si se llegó a 10 segundos y no se presionó el botón, se reinicia las cuentas.
                     segundos = 0;
                 }
                 break;
-            
             case 1:                
                 if (segundos < 3)               // Hacer que LDPV parpadee y LDPD encendida
                 {
                     state = 1;      
-                    if (count<50)
+                    if (count<50)               // LDPV está apagada en la primera mitad de cada segundo
                     {
                         PORTB = 0x20;
                     }
-                    if (count >= 50)
+                    if (count >= 50)            // LDPV se enciende en la segunda mitad de cada segundo
                     {
                         PORTB = 0x21;
                     }
@@ -79,71 +78,68 @@ int main()
                 }
                 else
                 {
-                    state = 2;
+                    state = 2;              // Se incrementa la cuenta cada segundo completado
                     count = 0;
                     segundos = 0;
                 }
                 break;
-
             case 2:
                 PORTB = 0x22;   // LDPD y LDVD encendidas
                 if (count == 100)   // Medir 1 segundo
                 {
-                    state = 3;
+                    state = 3;          // Se pasa al siguiente estado y se reinician las cuentas
                     count = 0;
                     segundos = 0;
                 }
                 else
                 {
-                    state = 2;
+                    state = 2;      // Si no ha transcurrido  un segundo se mantiene en el estado actual
                 }                
                 break;
             case 3:
                 PORTB = 0x12;   // LDPP y LDVD encendidas
                 state = 3;
-                if (count == 100)       // Medir 10 segundos
+                if (count == 100)       // Cada segundo se incrementa la cuenta de segundos y se reinicia count
                 {
                     segundos = segundos + 1;
                     count = 0;
                 }
-                if (segundos == 10)
+                if (segundos == 10)     // A los 10 segundos se pasa al siguiente estado y se reinician las cuentas
                 {
                     state = 4;
                     count = 0;
                     segundos = 0;
                 }                
                 break;
-
             case 4:
-                if (segundos < 3)               // Hacer que LDPP parpadee y LDVD encendida
+                if (segundos < 3)           // Se permanece en este estado por 3 segundos
                 {   
-                    state = 4;
+                    state = 4;              // LDPP permanece apagada en la primera mitad de cada segundo
                     if (count<50)
                     {
                         PORTB = 0x02;
                     }
-                    if (count>=50)
+                    if (count>=50)          // LDPP se enciende en la segunda mitad de cada segundo
                     {
                         PORTB = 0x12;
                     }
-                    if (count == 100)
+                    if (count == 100)       // Cada segundo se incrementa la cuenta
                     {
                         segundos = segundos + 1;
                         count = 0;
                     }
                 }
-                else
+                else                        // A los 3 segundos se pasa al siguiente estado y se restablecen las cuentas
                 {
                     state = 5;
                     count = 0;
                     segundos = 0;
                 }
                 break;
-            
-            case 5:
+            case 5:                     // Se encienden LDVD y LDPD
                 state = 5;
                 PORTB = 0x22;
-                if (count == 100)   // Medir 1 segundo
+                if (count == 100)   // Cuando ha transcurrido 1 segundo se pasa al siguiente estado y se reinician las cuentas
                 {
                     state = 0;
                     count = 0;
